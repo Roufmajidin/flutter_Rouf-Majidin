6c153681a44cab10fbe16585242b4aec392a8dc9
rouf_majidin <roufmajidin777@gmail.com>
2023-02-23 16:22:20 +0700

    add: menghitung keliling dan luas

--- 6_fundamental dart/praktikum/fundamental_dart/bin/soal_prioritas_1/keliling_persegi_panjang.dart
+++ 6_fundamental dart/praktikum/fundamental_dart/bin/soal_prioritas_1/keliling_persegi_panjang.dart
@@ -1,11 +1,13 @@
 void main() {
-  print("Keliling Persegi Panjang");
+  print("Menghitung Keliling Persegi Panjang");
   //rmus keliling persegi panjang
 // 2 * p + l
   int p = 20;
   int l = 40;
   var rumus = 2 * (p + l);
 
-  print(
-      "Dik : \n - panjang : $p \n - lebar : $l,\n Keliling Persegi panjang adalah : $rumus");
+  print(""" Diketahui : 
+      - panjang : $p
+      - lebar : $l,
+      Keliling Persegi panjang adalah : $rumus """);
 }
---
+++ 6_fundamental dart/praktikum/fundamental_dart/bin/soal_prioritas_1/luas_persegi_panjang.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Menghitung Luas Persegi Panjang");
+  //rmus keliling persegi panjang
+// 2 * p + l
+  int p = 20;
+  int l = 40;
+  var luas = p + l;
+
+  print("""Diketahui :
+       - panjang : $p 
+       - lebar : $l,
+       Luas Persegi panjang adalah : $luas""");
+}
