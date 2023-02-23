be22ea69039ee4f2448a99866016ed9c199cea3b
rouf_majidin <roufmajidin777@gmail.com>
2023-02-23 16:19:24 +0700

    add: sections 6

---
+++ 6_fundamental dart/praktikum/fundamental_dart/bin/fundamental_dart.dart
@@ -0,0 +1,5 @@
+import 'package:fundamental_dart/fundamental_dart.dart' as fundamental_dart;
+
+void main() {
+  // print('Hello world: ${fundamental_dart.calculate()}!');
+}
---
+++ 6_fundamental dart/praktikum/fundamental_dart/bin/soal_prioritas_1/keliling_persegi_panjang.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("Keliling Persegi Panjang");
+  //rmus keliling persegi panjang
+// 2 * p + l
+  int p = 20;
+  int l = 40;
+  var rumus = 2 * (p + l);
+
+  print(
+      "Dik : \n - panjang : $p \n - lebar : $l,\n Keliling Persegi panjang adalah : $rumus");
+}
