13adc750270e6f4398d4304e800a0d629e601c13
rouf_majidin <roufmajidin777@gmail.com>
2023-02-23 16:47:35 +0700

    add: soal prioritas2- gabung 3 variabel

---
+++ 6_fundamental dart/praktikum/fundamental_dart/bin/soal_prioritas_1/soal_prioritas_2/menggabungkan_3_variabel.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nama = "Rouf Majidin";
+  String jurusan = "Teknik Informatika";
+  String universitas = "Unviersitas Catur Insan Cendekia";
+
+  print("Hallo Nama saya $nama,\n Jurusan $jurusan, asal Kampus $universitas");
+}
