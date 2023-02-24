0a6097d82ec53898e98a880e28dff7bff420e73f
rouf_majidin <roufmajidin777@gmail.com>
2023-02-24 15:48:32 +0700

    add: readme.md

--- 6_fundamental dart/praktikum/fundamental_dart/bin/soal_eksplorasi/faktorial.dart
+++ 6_fundamental dart/praktikum/fundamental_dart/bin/soal_eksplorasi/faktorial.dart
@@ -2,7 +2,7 @@ void main() {
   int angka = 36;
   int faktorial = 1;
   for (int i = 1; i <= angka; i++) {
-    faktorial %= i;
+    faktorial *= i;
     // print(i);
     print(faktorial * i);
   }
